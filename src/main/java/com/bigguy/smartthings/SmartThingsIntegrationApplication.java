d77346af73d95b68b89c194233ff99b993fd87d4
jgathin.github.com <jgathin@gmail.com>
2025-09-14 14:39:54 -0500

    init commit

---
+++ src/main/java/com/bigguy/smartthings/SmartThingsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.bigguy.smartthings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartThingsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartThingsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigguy/smartthings/SmartThingsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigguy.smartthings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartThingsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
